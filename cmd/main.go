956c5efe38500e15ede36e479f8eddf7155c1df4
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2024-02-11 23:53:57 +0300

    add port config

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"Fibers-Example/routers"
 	"github.com/gofiber/fiber/v3"
+	"os"
 )
 
 func main() {
 	app := fiber.New()
 	routers.RouterV1{}.Bind(app)
-	err := app.Listen(":80")
+	port := os.Getenv("PORT")
+	err := app.Listen("0.0.0.0:" + port)
 	if err != nil {
 		return
 	}
