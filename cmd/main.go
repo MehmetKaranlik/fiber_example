818a004e5330051685e91a29c67399c849fa8df1
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2024-02-11 23:57:57 +0300

    add port config

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"Fibers-Example/routers"
+	"fmt"
 	"github.com/gofiber/fiber/v3"
 	"os"
+)
+
+var (
+	ip_addr = os.Getenv("IP")
 )
 
 func main() {
 	app := fiber.New()
 	routers.RouterV1{}.Bind(app)
 	port := os.Getenv("PORT")
-	err := app.Listen("0.0.0.0:" + port)
+	err := app.Listen(fmt.Sprintf("%s:%s", ip_addr, port))
 	if err != nil {
 		return
 	}
