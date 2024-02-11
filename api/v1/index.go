8e9a352a9e8000e3cb419127c3c19aed1432164b
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2024-02-11 23:19:50 +0300

    Complete index.go

--- api/v1/index.go
+++ api/v1/index.go
@@ -5,6 +5,5 @@ import "github.com/gofiber/fiber/v3"
 type IndexAPI struct{}
 
 func (i IndexAPI) Handler(ctx fiber.Ctx) error {
-	//TODO implement me
-	panic("implement me")
+	return ctx.SendString("Hello, World!")
 }
