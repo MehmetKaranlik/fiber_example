956f388cc1a664fa2cb6d3f91d675f9fb4cbe426
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2024-02-15 02:00:32 +0300

    Add router_v1.go

--- routers/router_v1.go
+++ routers/router_v1.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	v1 "Fibers-Example/api/v1"
+	"Fibers-Example/api/v1/auth"
 	"github.com/gofiber/fiber/v3"
 )
 
@@ -13,4 +14,5 @@ type RouterV1 struct{}
 
 func (self RouterV1) Bind(app *fiber.App) {
 	app.Get("/", v1.IndexAPI{}.Handler)
+	app.Post(API_PREFIX+"/auth/login", auth.LoginAPI{}.Handler)
 }
